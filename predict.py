33f96cd1a2035272e262fe53501e9b7b075bfa28
minju0307 <86367674+minju0307@users.noreply.github.com>
2022-01-07 15:46:58 +0900

    predict text

--- predict.py
+++ predict.py
@@ -13,3 +13,26 @@ model_path=os.path.join('model')
 model=ElectraForSequenceClassification.from_pretrained(model_path)
 tokenizer=AutoTokenizer.from_pretrained('monologg/koelectra-base-v3-discriminator')
 
+while True:
+    try:
+        text=get_word()
+        model.eval()
+
+        model_input=tokenizer.encode_plus(text)
+
+        input={k:torch.tensor([v], dtype=torch.long) for k, v in model_input.items()}
+        input['labels']=None
+
+        output=model(**input)
+        logits=output[0]
+
+        pred=logits.detach().cpu().numpy()
+        pred_idx=np.argmax(pred, axis=1)
+
+        label=label_list[int(pred_idx)]
+        print(f'{label}, {label_dict[label]}')
+
+    except Exception as e:
+        import traceback
+        print(e)
+        print(traceback.print_exc())
